a4fb32e6e1604de7451b81232d7c67dc743e4e28
Xiaolong Chen <chenxiaolong0502@163.com>
2021-11-20 19:47:32 +0800

    [fix] fix mrr bug to avoid out of bounds

--- torchrec/eval/__init__.py
+++ torchrec/eval/__init__.py
@@ -35,7 +35,8 @@ def ndcg(pred, target, k):
 def mrr(pred, target, k):
     row, col = torch.nonzero(pred[:, :k], as_tuple=True)
     row_uniq, counts = torch.unique_consecutive(row, return_counts=True)
-    idx = torch.cat([counts.new_zeros(1), counts.cumsum(dim=-1)[:-1]])
+    idx = torch.zeros_like(counts)
+    idx[1:] = counts.cumsum(dim=-1)[:-1]
     first = col.new_zeros(pred.size(0)).scatter_(0, row_uniq, col[idx]+1)
     output = 1.0 / first
     output[first==0] = 0
