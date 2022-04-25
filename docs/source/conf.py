88a2ee578991d1a2278c728d08782ac2f9f78246
AngusHuang17 <angus.xu.huang@foxmail.com>
2022-04-25 15:50:18 +0800

    [doc] config for read the docs [doc] config the stylesheet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -68,3 +66,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
