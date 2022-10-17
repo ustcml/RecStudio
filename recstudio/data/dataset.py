[feat&fix]  add some features and fix bugs. - add auto select gpu feature - add tensorboard logs - add last_x param in MLPModule - add arguments for SASRec - add gradient clip feature

recstudio/data/dataset.py
@@ -9,7 +9,7 @@ import pandas as pd
 import scipy.sparse as ssp
 import torch
 from recstudio.ann.sampler import (MaskedUniformSampler, PopularSamplerModel,
-                                   UniformSampler)
+                                   UniformSampler, uniform_sampling)
 from recstudio.utils import (DEFAULT_CACHE_DIR, check_valid_dataset, set_color,
                              download_dataset, md5, parser_yaml, get_dataset_default_config)
 from torch.nn.utils.rnn import pad_sequence
@@ -393,10 +393,6 @@ class MFDataset(Dataset):
 
     def _post_preprocess(self):
         if self.ftime in self.inter_feat:
-            # if self.field2type[self.ftime] == 'float':
-            #     self.inter_feat.sort_values(
-            #         by=[self.fuid, self.ftime], inplace=True)
-            #     self.inter_feat.reset_index(drop=True, inplace=True)
             if self.field2type[self.ftime] == 'str':
                 assert 'time_format' in self.config, "time_format is required when timestamp is string."
                 time_format = self.config['time_format']
@@ -404,16 +400,7 @@ class MFDataset(Dataset):
             elif self.field2type[self.ftime] == 'float':
                 pass
             else:
-                raise ValueError(
-                    f'The field [{self.ftime}] should be float or str type')
-
-            self.inter_feat.sort_values(
-                by=[self.fuid, self.ftime], inplace=True)
-            self.inter_feat.reset_index(drop=True, inplace=True)
-        else:
-            self.inter_feat.sort_values(
-                by=self.fuid, inplace=True)
-            self.inter_feat.reset_index(drop=True, inplace=True)
+                raise ValueError(f'The field [{self.ftime}] should be float or str type')
         self._prepare_user_item_feat()
 
     def _recover_unmapped_feature(self, feat):
@@ -469,12 +456,12 @@ class MFDataset(Dataset):
         keep &= item_list.isin(keep_items)
         self.inter_feat = self.inter_feat[keep]
         self.inter_feat.reset_index(drop=True, inplace=True)
-        # if self.user_feat is not None:
-        #    self.user_feat = self.user_feat[self.user_feat[self.fuid].isin(keep_users)]
-        #    self.user_feat.reset_index(drop=True, inplace=True)
-        # if self.item_feat is not None:
-        #    self.item_feat = self.item_feat[self.item_feat[self.fiid].isin(keep_items)]
-        #    self.item_feat.reset_index(drop=True, inplace=True)
+        if self.user_feat is not None:
+           self.user_feat = self.user_feat[self.user_feat[self.fuid].isin(keep_users)]
+           self.user_feat.reset_index(drop=True, inplace=True)
+        if self.item_feat is not None:
+           self.item_feat = self.item_feat[self.item_feat[self.fiid].isin(keep_items)]
+           self.item_feat.reset_index(drop=True, inplace=True)
 
     def get_graph(self, idx, form='coo', value_fields=None, row_offset=0, col_offset=0, bidirectional=False, shape=None):
         """
@@ -738,6 +725,10 @@ class MFDataset(Dataset):
             data.update(self.user_feat[uid])
             data.update(self.item_feat[iid])
 
+        if 'user_hist' in data:
+            user_count = self.user_count[data[self.fuid]].max()
+            data['user_hist'] = data['user_hist'][:, 0:user_count]
+
         return data
 
     def __getitem__(self, index):
@@ -756,8 +747,10 @@ class MFDataset(Dataset):
             if getattr(self, 'neg_sampling_count', None) is not None:
                 user_count = self.user_count[data[self.fuid]].max()
                 user_hist = self.user_hist[data[self.fuid]][:, 0:user_count]
-                _, neg_id, _ = self.negative_sampler(
-                    data[self.fuid].view(-1, 1), self.neg_sampling_count, user_hist)
+                neg_id = uniform_sampling(index.size(0), self.num_items,
+                                          self.neg_sampling_count, user_hist)
+                # _, neg_id, _ = self.negative_sampler(
+                # data[self.fuid].view(-1, 1), self.neg_sampling_count, user_hist)
                 neg_item_feat = self.item_feat[neg_id.long()]
                 for k in neg_item_feat:
                     data['neg_'+k] = neg_item_feat[k]
@@ -814,13 +807,21 @@ class MFDataset(Dataset):
         if drop_dup:
             self.inter_feat = self.inter_feat[self.first_item_idx]
 
+        if (split_mode == 'user_entry') or (split_mode == 'user'):
+            if self.ftime in self.inter_feat:
+                self.inter_feat.sort_values(by=[self.fuid, self.ftime], inplace=True)
+                self.inter_feat.reset_index(drop=True, inplace=True)
+            else:
+                self.inter_feat.sort_values(by=self.fuid, inplace=True)
+                self.inter_feat.reset_index(drop=True, inplace=True)
+
         if split_mode == 'user_entry':
             user_count = self.inter_feat[self.fuid].groupby(
                 self.inter_feat[self.fuid], sort=False).count()
             if shuffle:
                 cumsum = np.hstack([[0], user_count.cumsum()[:-1]])
-                idx = np.concatenate([np.random.permutation(
-                    c) + start for start, c in zip(cumsum, user_count)])
+                idx = np.concatenate([np.random.permutation(c)
+                        + start for start, c in zip(cumsum, user_count)])
                 self.inter_feat = self.inter_feat.iloc[idx].reset_index(
                     drop=True)
         elif split_mode == 'entry':
@@ -921,8 +922,8 @@ class MFDataset(Dataset):
             raise ValueError('can not compute sample length for this dataset')
 
     def eval_loader(self, batch_size, num_workers=1, ddp=False):
+        self.eval_mode = True
         if not getattr(self, 'fmeval', False):
-            self.eval_mode = True
             # if ddp:
             #     sampler = torch.utils.data.distributed.DistributedSampler(self, shuffle=False)
             #     output = DataLoader(

recstudio/model/module/ctr.py
@@ -2,7 +2,7 @@ from typing import Set
 
 import torch
 import torch.nn as nn
-from recstudio.model.module import SeqPoolingLayer
+from .layers import MLPModule, AttentionLayer, SeqPoolingLayer
 
 
 class DenseEmbedding(torch.nn.Module):
@@ -218,8 +218,10 @@ class DINScorer(torch.nn.Module):
 
 
 class BehaviorSequenceTransformer(nn.Module):
-    def __init__(self, fuid, fiid, num_users, num_items, max_len, embed_dim, hidden_size, n_layer, n_head, dropout,
-                 mlp_layers=[1024, 512, 256], activation='relu', batch_first=True, norm_first=False):
+    def __init__(self, fuid, fiid, num_users, num_items, max_len,
+                 embed_dim, hidden_size, n_layer, n_head, dropout,
+                 mlp_layers=[1024, 512, 256], activation='leakyrelu',
+                 batch_first=True, norm_first=False):
         super().__init__()
         self.fuid = fuid
         self.fiid = fiid
@@ -227,26 +229,29 @@ class BehaviorSequenceTransformer(nn.Module):
         self.item_embedding = torch.nn.Embedding(num_items, embed_dim, 0)
         self.position_embedding = torch.nn.Embedding(max_len+2, embed_dim, 0)
         tfm_encoder = torch.nn.TransformerEncoderLayer(
-            d_model=embed_dim, nhead=n_head, dim_feedforward=hidden_size, dropout=dropout, activation=activation,
-            batch_first=batch_first, norm_first=norm_first)
+            d_model=embed_dim, nhead=n_head, dim_feedforward=hidden_size,
+            dropout=dropout, activation='relu', batch_first=batch_first,
+            norm_first=norm_first)
         self.transformer = torch.nn.TransformerEncoder(
             encoder_layer=tfm_encoder, num_layers=n_layer)
-        self.mlp = MLPModule([(max_len+1)*embed_dim, ] +
-                             mlp_layers, activation_func=activation)
+        self.mlp = MLPModule([(max_len+1)*embed_dim, ] + mlp_layers,
+                             activation_func = activation,
+                             dropout = dropout)
         self.predict = torch.nn.Linear(mlp_layers[-1], 1)
 
     def forward(self, batch):
         hist = batch['in_'+self.fiid]
         target = batch[self.fiid]
         seq_len = batch['seqlen']
-        hist = torch.cat((hist, target.view(-1, 1)), dim=1)
+        hist = torch.cat((hist, torch.zeros_like(target.view(-1, 1))), dim=1)
         B, L = hist.shape
+        idx_ = torch.arange(0, B, dtype=torch.long)
+        hist[idx_, seq_len] = target.long()
         seq_emb = self.item_embedding(hist)
         positions = torch.arange(1, L+1, device=seq_emb.device)
         positions = torch.tile(positions, (B,)).view(B, -1)
         padding_mask = hist == 0
         positions[padding_mask] = 0
-        positions[:, -1] = seq_len + 1
         position_emb = self.position_embedding(positions)
         attention_mask = torch.triu(torch.ones(
             (L, L), dtype=torch.bool, device=hist.device), 1)

recstudio/model/seq/din.py
@@ -32,7 +32,7 @@ class DIN(basemodel.BaseRanker):
         parent_parser.add_argument("--activation", type=str, default='dice', help='activation for MLP')
         parent_parser.add_argument("--attention_mlp", type=int, nargs='+', default=[128, 64], help='MLP layer size for attention calculation')
         parent_parser.add_argument("--fc_mlp", type=int, nargs='+', default=[128, 64, 64], help='MLP layer size for the MLP before prediction')
-        parent_parser.add_argument("--negativate_count", type=int, default=1, help='negative sampling numbers')
+        parent_parser.add_argument("--negative_count", type=int, default=1, help='negative sampling numbers')
         parent_parser.add_argument("--dropout", type=float, default=0.3, help='dropout rate for MLP')
         return parent_parser
 
@@ -56,7 +56,7 @@ class DIN(basemodel.BaseRanker):
 
 
     def _get_scorer(self, train_data):
-        return module.DINScorer(
+        return module.ctr.DINScorer(
             train_data.fuid, train_data.fiid, train_data.num_users, train_data.num_items,
             self.embed_dim, self.config['attention_mlp'], self.config['fc_mlp'], dropout=self.config['dropout'],
             activation=self.config['activation'], batch_norm=False
@@ -65,8 +65,7 @@ class DIN(basemodel.BaseRanker):
 
     def _get_loss_func(self):
         r"""BinaryCrossEntropy is used as the loss function."""
-        # return loss_func.BinaryCrossEntropyLoss()
-        return loss_func.SampledSoftmaxLoss()
+        return loss_func.BinaryCrossEntropyLoss()
 
     @torch.no_grad()
     def topk(self, batch, k, user_h):

recstudio/utils/utils.py
@@ -6,7 +6,7 @@ import os
 import random
 import re
 from collections import OrderedDict
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 
 import numpy as np
 import requests
@@ -18,7 +18,6 @@ from recstudio import LOG_DIR, DEFAULT_CACHE_DIR
 
 # LOG_DIR = r"./log/"
 # DEFAULT_CACHE_DIR = r"./.recstudio/"
-
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
@@ -112,7 +111,7 @@ def get_model(model_name: str):
     Returns:
         Recommender: model class
     """
-    model_submodule = ['ae', 'mf', 'seq', 'fm', 'graph', 'kg']
+    model_submodule = ['ae', 'mf', 'seq', 'fm', 'graph', 'kg', 'debias']
 
     model_file_name = model_name.lower()
     model_module = None
@@ -371,3 +370,31 @@ def get_logger(file_path: str = None):
     if file_path is not None:
         logger = add_file_handler(logger, file_path, formatter)
     return logger
+
+def get_gpus(gpu_config: Union[int, list]):
+    if gpu_config is None:
+        return None
+    if isinstance(gpu_config, int):
+        # select k most free gpus
+        try:
+            import nvidia_smi
+        except ModuleNotFoundError:
+            raise ModuleNotFoundError("Package 'nvidia-ml-py3' not found, please "
+                "refer to https://pypi.org/project/nvidia-ml-py3 to install.")
+        nvidia_smi.nvmlInit()
+        total_gpu_nums = nvidia_smi.nvmlDeviceGetCount()
+        all_gpu_info = np.zeros((2, total_gpu_nums))
+        for i in range(total_gpu_nums):
+            handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+            info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+            all_gpu_info[0, i] = info.free/(1024**3)
+            all_gpu_info[1, i] = info.total/(1024**3)
+        nvidia_smi.nvmlShutdown()
+        sorted_free_gpus = np.argsort(all_gpu_info[0])[::-1]
+        return list(sorted_free_gpus[:gpu_config])
+
+    elif isinstance(gpu_config, list):
+        return gpu_config
+
+    else:
+        raise TypeError(f'expected `gpu_config` to be int or list, but got {type(gpu_config)}.')